0254a4f02b99615b2075fa773c1123915cb89ea9
Ararat <ararat.martirossyan@jumbo.com>
2021-02-21 16:35:31 +0100

    Test autodeploy

--- src/services/generic.service.js
+++ src/services/generic.service.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = 'https://mevn.incodewetrust.ru/api/v1'
+const API_URL = process.env.VUE_APP_API_URL
 
 export const request = async ({ url, method, data = {} }) => {
   const response = await axios[method](`${API_URL}/${url}`, data)
